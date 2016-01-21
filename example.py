aeb5e417cc1061bc3175c572e1bb40674ef4376b
xjq <xjq314@gmail.com>
2016-01-21 21:24:51 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from flask import request
 
 app = (__name__)
 
 @app.route('/auth')
 def authenticate():
-  return 'Hello world'
+  
+  token = 'xxxxxx'
+  
+  signature = request.args.get('signature')
+  timestamp = request.args.get('timestamp')
+  nonce = request.args.get('nonce')
+  echostr = request.args.get('echostr')
+  
+  triplet = [token, timestamp, nonce]
+  triplet.sort()
+  tmpstr =hash(triplet[1] + triplet[2] + triplet[3])
+  
+  if (echostr == tempstr):
+    return echostr
+  else:
+    return 'Fuch you'
   
 if __name__ == '__main__':
   app.debug = True
