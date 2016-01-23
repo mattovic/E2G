f38ec9982c786fbfc226fb80651c99bb549d60c3
mattovic <viiup@vip.qq.com>
2016-01-23 14:20:20 +0800

    added auto_reply function
    added auto_reply function which could make wechat replies you as same as what you input (text).

--- auth.py
+++ auth.py
@@ -1,29 +1,35 @@
 # -*- coding: utf-8 -*-
 
 import hashlib
-from flask import Flask
-from flask import request
+import time
+from flask import Flask, request, make_response
+from xml.etree import ElementTree as ET
 
 app = Flask(__name__)
 
-@app.route('/auth')
+@app.route('/', methods=['GET', 'POST'])
 def authenticate():
-    token = 'brandnewday'
-    
-    signature = request.args['signature']
-    timestamp = request.args['timestamp']
-    nonce = request.args['nonce']
-    echostr = request.args['echostr']
-    
-    triplet = sorted([token, timestamp, nonce])
-    sha1 = hashlib.sha1()
-    tempstr = sha1.update(triplet[0] + triplet[1] + triplet[2])
-    hashcode = sha1.hexdigest()
-
-    if signature == hashcode:
-        return echostr
+    if request.method == 'GET':
+        token = 'brandnewday'
+    	signature = request.args['signature']
+    	timestamp = request.args['timestamp']
+    	nonce = request.args['nonce']
+    	echostr = request.args['echostr']
+    	triplet = sorted([token, timestamp, nonce])
+    	sha1 = hashlib.sha1()
+    	tempstr = sha1.update(triplet[0] + triplet[1] + triplet[2])
+    	hashcode = sha1.hexdigest()
+    	if signature == hashcode:
+        	return echostr
     else:
-        return False
+    	xmlMsg = ET.fromstring(request.data)
+    	toUser = xmlMsg.find("ToUserName").text
+    	fromUser =  xmlMsg.find("FromUserName").text
+    	content = xmlMsg.find("Content").text
+    	reply = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content></xml>"
+    	response = make_response(reply % (fromUser, toUser, int(time.time()), content))
+        response.content_type = 'application/xml'
+    	return response
 
 if __name__ == '__main__':
     app.debug = True
